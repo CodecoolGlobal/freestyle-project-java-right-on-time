1bd9e8b18d1fbdd290bcb26c7f64db1fe21cf412
Rónai Zsuzsanna <ronai.zsuzsanna@gmail.com>
2019-05-16 01:01:16 +0200

    main with menu - run not working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,49 @@
 package com.company;
 
 import com.codecool.termlib.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+	public static Integer userChoice;
+	public static String[] arrayOfUserOptions = new String[] { "draw rectangle", "draw triangle", "draw HUN flag", "draw UKR flag", "draw heart", "draw Donald Duck" };
+	public static String[] arrayOfMethods = new String[] { "Rectangle.drawRectangle()", "Triangle.drawTriangle()", "Command.drawHungarianFlag()", "Command.drawUkrainianFlag()"};
+//, Command.drawHeart(), Command.drawDonald() };
 
-
-public static void main(String[] args) {
-
-	Rectangle.drawRectangle();
-	//Triangle.drawTriangle();
-	Command.drawHungarianFlag();
+	public static void showOptions() {
+		Terminal.setUnderline();
+		System.out.println("Main menu"+"\n");
+		Terminal.resetStyle();
+		for (String option : arrayOfUserOptions) {
+		System.out.println(Arrays.asList(arrayOfUserOptions).indexOf(option)+1 + ". " + option);
+		}
 	}
+
+	public static Integer getUserChoice() {
+    		boolean bool;
+		//Integer userChoice = Integer.valueOf(getUserChoice())-1;
+		do {
+		System.out.print("\n" + "Please choose from the options above: ");
+		Scanner scanner = new Scanner(System.in);
+		userChoice = scanner.nextInt();
+		bool = userChoice > 0 && userChoice <= arrayOfUserOptions.length;
+    		}
+		while (!bool);
+		return userChoice;
+	}
+
+	public static String getMethod(Integer userChoice) { 
+		return String.valueOf(arrayOfMethods[userChoice-1]);
+	}
+
+	public static void main(String[] args) {
+	showOptions();
+	userChoice = getUserChoice();
+	System.out.println(getMethod(userChoice));
+	}
+
+	//Rectangle.drawRectangle();
+	//Triangle.drawTriangle();
+	//Command.drawHungarianFlag();
+	
 }
