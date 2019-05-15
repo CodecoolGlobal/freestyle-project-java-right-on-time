e811403789164606a2876dfb5cd3e0e6c1661250
Rónai Zsuzsanna <ronai.zsuzsanna@gmail.com>
2019-05-15 14:39:18 +0200

    main changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,8 @@ public class Main {
 
 
 public static void main(String[] args) {
-	//Terminal.getCursorPosition();
-	//Terminal.setColor(Color.GREEN);
-	Terminal.setChar('a');
-	Terminal.moveCursor(Direction.FORWARD, 10);
-	/*Terminal.setChar('x');
-	Terminal.setBgColor(Color.YELLOW);
-	System.out.println("newgreen");
-	Terminal.setUnderline();
-	System.out.println("underlinegreen");
-	Terminal.resetStyle();
-	System.out.println("reseted");
-	Terminal.clearScreen();*/
-	//Terminal.moveTo(5, 100);
-	//Terminal.moveCursor(Direction.FORWARD, 12);
-	//System.out.print("Something");
+
+	Rectangle.drawRectangle(10, Color.YELLOW);
 	}
 
 }
