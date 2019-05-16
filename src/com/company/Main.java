c77fcfd8d0beb277c3bdfd34f94c2db76765e458
Rónai Zsuzsanna <ronai.zsuzsanna@gmail.com>
2019-05-16 12:38:43 +0200

    refactor main menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,21 @@
 package com.company;
 
 import com.codecool.termlib.*;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
 	public static Integer userChoice;
 	public static String[] arrayOfUserOptions = new String[] { "draw square", "draw rectangle", "draw triangle", "draw flags", "draw heart", "draw Donald Duck", "exit" };
 	public static Runnable[] arrayOfMethods = {Square::drawSquare, Rectangle::drawRectangle, Triangle::drawTriangle, FlagOptions::showOptions, Command::drawHeart, Command::drawDonald, () -> System.exit(0) };
 
-	public static void showOptions() {
-		Terminal.setUnderline();
-		System.out.println("Main menu"+"\n");
-		Terminal.resetStyle();
-		for (String option : arrayOfUserOptions) {
-		System.out.println(Arrays.asList(arrayOfUserOptions).indexOf(option)+1 + ". " + option);
-		}
-	}
-
-	public static Integer getUserChoice() {
-    		boolean bool;
-		do {
-		System.out.print("\n" + "Please choose from the options above: ");
-		Scanner scanner = new Scanner(System.in);
-		userChoice = scanner.nextInt();
-		bool = userChoice > 0 && userChoice <= arrayOfUserOptions.length;
-    		}
-		while (!bool);
-		return userChoice;
-	}
-
-	public static void runUserChoice (int userChoice) {
-		arrayOfMethods[userChoice].run();
-	}
 
 	public static void main(String[] args) {
 		while (true) {			
-			showOptions();
-			userChoice = getUserChoice();
+			Menu.showOptions("Main menu", arrayOfUserOptions);
+			userChoice = Menu.getUserChoice(arrayOfUserOptions.length);
 			Terminal.clearScreen();
 			Terminal.moveTo(1,1);
-			runUserChoice(userChoice-1);
+			Menu.runUserChoice(userChoice-1, arrayOfMethods);
 		}
 	}	
 }
---
+++ src/com/company/Menu.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import com.codecool.termlib.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Menu {
+	public static Integer userChoice;
+	
+
+	public static void showOptions(String menuTitle, String[] arrayOfUserOptions) {
+		Terminal.setUnderline();
+		System.out.println(menuTitle+"\n");
+		Terminal.resetStyle();
+		for (String option : arrayOfUserOptions) {
+		System.out.println(Arrays.asList(arrayOfUserOptions).indexOf(option)+1 + ". " + option);
+		}
+	}
+
+	public static Integer getUserChoice(int arrayOfOptionsLength) {
+    		boolean bool;
+		do {
+		System.out.print("\n" + "Please choose from the options above: ");
+		Scanner scanner = new Scanner(System.in);
+		userChoice = scanner.nextInt();
+		bool = userChoice > 0 && userChoice <= arrayOfOptionsLength;
+    		}
+		while (!bool);
+		return userChoice;
+	}
+
+	public static void runUserChoice (int userChoice, Runnable[] arrayOfMethods) {
+		arrayOfMethods[userChoice].run();
+	}
+
+}
+
